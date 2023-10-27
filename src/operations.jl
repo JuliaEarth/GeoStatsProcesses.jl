914a5aa8374a7727d6a845c234180cd218c85a0b
Elias Carvalho <eliascarvdev@gmail.com>
2023-10-27 10:58:24 -0300

    Apply suggestions

--- src/operations.jl
+++ src/operations.jl
@@ -21,3 +21,25 @@ function thin end
 #-----------------
 
 include("thinning/random.jl")
+
+#---------------
+# PROCESS UNION
+#---------------
+
+"""
+    p₁ ∪ p₂
+
+Return the union of point processes `p₁` and `p₂`.
+"""
+Base.union(p₁::PointProcess, p₂::PointProcess) = UnionProcess(p₁, p₂)
+
+Base.union(p₁::PoissonProcess{<:Real}, p₂::PoissonProcess{<:Real}) = PoissonProcess(p₁.λ + p₂.λ)
+
+Base.union(p₁::PoissonProcess{<:Function}, p₂::PoissonProcess{<:Function}) = PoissonProcess(x -> p₁.λ(x) + p₂.λ(x))
+
+Base.union(p₁::PoissonProcess{<:Real}, p₂::PoissonProcess{<:Function}) = PoissonProcess(x -> p₁.λ + p₂.λ(x))
+
+Base.union(p₁::PoissonProcess{<:Function}, p₂::PoissonProcess{<:Real}) = PoissonProcess(x -> p₁.λ(x) + p₂.λ)
+
+Base.union(p₁::PoissonProcess{<:AbstractVector}, p₂::PoissonProcess{<:AbstractVector}) =
+  PoissonProcess(x -> p₁.λ + p₂.λ)
