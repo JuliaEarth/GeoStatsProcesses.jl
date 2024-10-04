3c44b3f5d0977a35ed9a5d28e1e59dbfef615605
Elias Carvalho <eliascarvdev@gmail.com>
2024-10-04 16:23:58 -0300

    Move 'Ensemble' and 'InitMethod's & Remove GeoStatsBase.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,14 +5,17 @@ using GeoTables
 using GeoStatsFunctions
 using GeoStatsImages
 using LinearAlgebra
-using Test, StableRNGs
+using Test, StableRNGs, CSV
 
 import ImageQuilting
 import TuringPatterns
 using StratiGraphics: SmoothingProcess, Environment, ExponentialDuration
 
+# environment settings
+datadir = joinpath(@__DIR__, "data")
+
 # list of tests
-testfiles = ["point.jl", "field.jl"]
+testfiles = ["point.jl", "field.jl", "ensembles.jl", "initbuff.jl"]
 
 @testset "GeoStatsProcesses.jl" begin
   for testfile in testfiles
