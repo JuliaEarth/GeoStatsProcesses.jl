8344a846af8bace830ca211f62c25df675b685f6
Júlio Hoffimann <julio.hoffimann@gmail.com>
2025-02-14 16:21:18 -0300

    Fix SEQSIM

--- src/simulation/field/gaussian/seq.jl
+++ src/simulation/field/gaussian/seq.jl
@@ -2,13 +2,19 @@
 # Licensed under the MIT License. See LICENSE in the project root.
 # ------------------------------------------------------------------
 
-function preprocess(::AbstractRNG, process::GaussianProcess, method::SEQSIM, domain, data)
+function preprocess(::AbstractRNG, process::GaussianProcess, method::SEQSIM, init, domain, data)
   # function and mean
   f = process.func
   μ = process.mean
 
+  # method options
+  minneighbors = method.minneighbors
+  maxneighbors = method.maxneighbors
+  neighborhood = method.neighborhood
+  distance = method.distance
+
   # scale domains for numerical stability
-  finv, dom, data = _scaledomains(domain, data)
+  finv, dom, dat = _scaledomains(domain, data)
 
   # scale function accordingly
   f̄ = GeoStatsFunctions.scale(f, finv)
@@ -23,13 +29,15 @@ function preprocess(::AbstractRNG, process::GaussianProcess, method::SEQSIM, dom
   end
 
   # determine probability model
-  probmodel = Kriging(f̄, μ)
-  marginal = Normal(μ, √sill(f̄))
+  μ̄, σ̄ = μ, _std(f̄)
+  probmodel = Kriging(f̄, μ̄)
+  gaussians = map((μ̄ᵢ, σ̄ᵢ) -> Normal(μ̄ᵢ, σ̄ᵢ), μ̄, σ̄)
+  marginal = product_distribution(gaussians)
 
   # adjust min/max neighbors
-  nobs = nelements(dom)
-  if maxneighbors > nobs || maxneighbors < 1
-    maxneighbors = nobs
+  nelm = nelements(dom)
+  if maxneighbors > nelm || maxneighbors < 1
+    maxneighbors = nelm
   end
   if minneighbors > maxneighbors || minneighbors < 1
     minneighbors = 1
@@ -44,71 +52,86 @@ function preprocess(::AbstractRNG, process::GaussianProcess, method::SEQSIM, dom
     KBallSearch(dom, maxneighbors, neigh)
   end
 
-  (; dom, data, probmodel, marginal, minneighbors, maxneighbors, searcher)
+  (; dom, dat, init, probmodel, marginal, minneighbors, maxneighbors, searcher)
 end
 
-function randsingle(rng::AbstractRNG, ::GaussianProcess, method::SEQSIM, domain, data, preproc)
-  # retrieve parameters
-  (; path, init) = method
-  (; dom, data, probmodel, marginal, minneighbors, maxneighbors, searcher) = preproc
+function randsingle(rng::AbstractRNG, process::GaussianProcess, method::SEQSIM, domain, data, preproc)
+  # retrieve preprocessing results
+  (; dom, dat, init, probmodel, marginal, minneighbors, maxneighbors, searcher) = preproc
 
-  # initialize buffers for realization and simulation mask
-  vars = Dict(zip(varnames, vartypes))
-  buff, mask = initbuff(dom, vars, init, data=data)
+  # method options
+  path = method.path
+
+  # initialize realization and mask
+  real, mask = randinit(process, dom, dat, init)
+
+  # retrieve variable names
+  vars = keys(real)
 
   # consider point set with centroids for now
   pointset = PointSet([centroid(dom, ind) for ind in 1:nelements(dom)])
 
-  pairs = map(varnames) do var
-    # pre-allocate memory for neighbors
-    neighbors = Vector{Int}(undef, maxneighbors)
+  # pre-allocate memory for neighbors
+  neighbors = Vector{Int}(undef, maxneighbors)
 
-    # retrieve realization and mask for variable
-    realization = buff[var]
-    simulated = mask[var]
+  # matrix buffer for realization
+  realization = stack(Tables.rowtable(real))
 
-    # simulation loop
-    for ind in traverse(dom, path)
-      if !simulated[ind]
-        center = pointset[ind]
-        # search neighbors with simulated data
-        nneigh = search!(neighbors, center, searcher, mask=simulated)
+  # locations with all variables already simulated
+  simulated = map(all, Tables.rowtable(mask))
 
-        if nneigh < minneighbors
-          # draw from marginal
-          realization[ind] = rand(rng, marginal)
-        else
-          # neighborhood with data
-          neigh = let
-            ninds = view(neighbors, 1:nneigh)
-            dom = view(pointset, ninds)
-            val = view(realization, ninds)
-            tab = (; var => val)
-            georef(tab, dom)
-          end
+  # simulation loop
+  @inbounds for ind in traverse(dom, path)
+    if !simulated[ind]
+      # center of target location
+      center = pointset[ind]
 
-          # fit distribution probmodel
-          fitted = GeoStatsModels.fit(probmodel, neigh)
+      # search neighbors with simulated data
+      nneigh = search!(neighbors, center, searcher, mask=simulated)
 
-          # draw from conditional or marginal
-          distribution = if GeoStatsModels.status(fitted)
-            GeoStatsModels.predictprob(fitted, var, center)
-          else
-            marginal
-          end
-          realization[ind] = rand(rng, distribution)
+      if nneigh < minneighbors
+        # draw from marginal
+        realization[:,ind] .= rand(rng, marginal)
+      else
+        # neighborhood with data
+        neigh = let
+          ninds = view(neighbors, 1:nneigh)
+          ndom = view(pointset, ninds)
+          nmat = view(realization, :, ninds)
+          ntab = (; zip(vars, eachrow(nmat))...)
+          # ntab = (; vars[1] => nmat[1,:])
+          georef(ntab, ndom)
         end
 
-        # mark location as simulated and continue
-        simulated[ind] = true
-      end
-    end
+        # fit probabilistic model
+        fitted = GeoStatsModels.fit(probmodel, neigh)
 
-    var => realization
+        # draw from conditional
+        conditional = if GeoStatsModels.status(fitted)
+          dists = GeoStatsModels.predictprob(fitted, vars, center)
+          product_distribution(dists)
+        else
+          marginal
+        end
+        realization[:,ind] .= rand(rng, conditional)
+      end
+
+      # mark location as simulated and continue
+      simulated[ind] = true
+    end
   end
 
-  (; pairs...)
+  # convert back to table format
+  @inbounds for (i, var) in enumerate(vars)
+    real[var] .= realization[i,:]
+  end
+
+  real
 end
+
+# -----------------
+# HELPER FUNCTIONS
+# -----------------
 
 function _scaledomains(domain, data)
   sdom = domain
@@ -131,3 +154,7 @@ function _scalefactor(domain)
   cmax = abs.(to(pmax))
   ustrip(max(cmin..., cmax...))
 end
+
+_std(f) = ustrip.(sqrt.(_diag(sill(f))))
+_diag(M::Matrix) = diag(M)
+_diag(M) = M
