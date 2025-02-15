db410b6c9b18ab400607328376e8c7cab3bd905c
Júlio Hoffimann <julio.hoffimann@gmail.com>
2025-02-15 08:10:58 -0300

    Improve docstrings

--- src/processes/field.jl
+++ src/processes/field.jl
@@ -5,8 +5,8 @@
 """
     FieldProcess <: GeoStatsProcess
 
-A field process (a.k.a. stochastic random field) is defined over a
-fixed geospatial domain. The realizations of the process are stored
+A field process (or random field) is defined over a fixed
+geospatial domain. The realizations of the process are stored
 in an ensemble, which is an indexable collection of geotables.
 """
 abstract type FieldProcess <: GeoStatsProcess end
