44a0fa29682052254a601080ba3711681eb4cf86
Júlio Hoffimann <julio.hoffimann@gmail.com>
2025-10-30 19:22:15 -0300

    Refactor aslen

--- src/utils.jl
+++ src/utils.jl
@@ -8,9 +8,9 @@
 
 const Len{T} = Quantity{T,u"𝐋"}
 
-aslen(x::Number) = x * u"m"
 aslen(x::Len) = x
-aslen(::Quantity) = throw(ArgumentError("invalid units, please check the documentation"))
+aslen(x::Number) = x * u"m"
+aslen(::Quantity) = throw(ArgumentError("invalid length unit"))
 
 # ---
 # IO
